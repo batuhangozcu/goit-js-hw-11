9030e481dc9a88251f89605198861c12a444d38f
batuhangozcu <batuhangozcu1@gmail.com>
2025-01-19 23:40:53 +0300

    refreshFixed

--- src/js/task.js
+++ src/js/task.js
@@ -9,7 +9,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 // Stil importu
 import "izitoast/dist/css/iziToast.min.css";
-import { Input } from "postcss";
 
 const input = document.getElementById("input");
 const button = document.getElementById("submitButton");
@@ -23,7 +22,9 @@ const params = new URLSearchParams({
   per_page: "9",
 });
 
-button.addEventListener("click", () => {
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  lists.innerHTML = "";
   params.set("q", input.value);
   fetch(`https://pixabay.com/api/?${params}`)
     .then((r) => r.json())
@@ -39,7 +40,7 @@ button.addEventListener("click", () => {
         .map((photo) => {
           return `<li class="list">
        <div class="cards">
-       <a class="link" href= "${photo.tags}">
+       <a class="link" href= "${photo.largeImageURL}">
        <img
         class="image"
         src="${photo.webformatURL}"
@@ -58,28 +59,6 @@ button.addEventListener("click", () => {
         })
         .join("");
       lists.insertAdjacentHTML("afterbegin", markup);
-    });
+    })
+    .catch((error) => console.log(error));
 });
-
-// "id": 4611189,
-//             "pageURL": "https://pixabay.com/photos/kitten-cat-pet-feline-animal-fur-4611189/",
-//             "type": "photo",
-//             "tags": "kitten, cat, pet, feline, animal, fur, kitty, domestic, domestic cat, cat portrait, animal world, nature, cat, cat, cat, cat, cat",
-//             "previewURL": "https://cdn.pixabay.com/photo/2019/11/08/11/56/kitten-4611189_150.jpg",
-//             "previewWidth": 150,
-//             "previewHeight": 99,
-//             "webformatURL": "https://pixabay.com/get/g1ba66902ba81fdb9d7ce81ed6370a8eeeea66ec3ecd1a36aff9c68582a039fa7468c89fc8f32c0984149a8eef758b6ed960c57f15f4ad90b95deb0ae69f31cd6_640.jpg",
-//             "webformatWidth": 640,
-//             "webformatHeight": 426,
-//             "largeImageURL": "https://pixabay.com/get/g042e49262bb34d0a395ae7e4d196b83db18fd58031cd608ac7703b090a36fb6e9b64bb421af1543a8a8c1fedf79b561cbd1859690fff2cbeb882b61fa6f4f062_1280.jpg",
-//             "imageWidth": 6000,
-//             "imageHeight": 4000,
-//             "imageSize": 3885974,
-//             "views": 524471,
-//             "downloads": 408576,
-//             "collections": 14119,
-//             "likes": 1139,
-//             "comments": 188,
-//             "user_id": 9285194,
-//             "user": "guvo59",
-//             "userImageURL": ""
