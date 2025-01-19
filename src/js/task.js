89f634cf2091c5fa4a36eca381f2bc585268d9dc
batuhangozcu <batuhangozcu1@gmail.com>
2025-01-19 22:10:42 +0300

    imagesListAdded

--- src/js/task.js
+++ src/js/task.js
@@ -11,8 +11,9 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import { Input } from "postcss";
 
-const input = document.getElementById("input")
-const button = document.getElementById("submitButton")
+const input = document.getElementById("input");
+const button = document.getElementById("submitButton");
+const lists = document.querySelector(".lists");
 
 const params = new URLSearchParams({
   key: "48294638-370103394c700755fbc6c4620",
@@ -22,113 +23,56 @@ const params = new URLSearchParams({
 });
 
 button.addEventListener("click", () => {
-    params.set("q", input.value)
-    fetch(`https://pixabay.com/api/?${params}`).then((r) => console.log(r));
-})
+  params.set("q", input.value);
+  fetch(`https://pixabay.com/api/?${params}`)
+    .then((r) => r.json())
+    .then((a) => {
+      if (a.total == 0) {
+        iziToast.error({
+          position: "topRight",
+          message:
+            "Sorry, there are no images matching your search query. Please try again!",
+        });
+      }
+      const markup = a.hits
+        .map((photo) => {
+          return `<li class="list">
+       <a class="link" href= "${photo.tags}">
+       <img
+        class="image"
+        src="${photo.previewURL}"
+        data-source="${photo.largeImageURL}"
+        alt="${photo.tags}"
+      />
+       </a>
+       </li>`;
+        })
+        .join("");
+      lists.insertAdjacentHTML("afterbegin", markup);
+    });
+});
 
 
 
-// const images = [
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg>",
-//     description: "Hokkaido Flower",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg>",
-//     description: "Container Haulage Freight",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg>",
-//     description: "Aerial Beach View",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg>",
-//     description: "Flower Blooms",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg>",
-//     description: "Alpine Mountains",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg>",
-//     description: "Mountain Lake Sailing",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg>",
-//     description: "Alpine Spring Meadows",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg>",
-//     description: "Nature Landscape",
-//   },
-//   {
-//     preview:
-//       "<https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg>",
-//     original:
-//       "<https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg>",
-//     description: "Lighthouse Coast Sea",
-//   },
-// ];
-
-// const gallery = document.querySelector(".gallery");
-
-// images.map((link) => {
-//   const lists = `<li class="gallery-item">
-//     <a class="gallery-link" href="${link.original.slice(1, -1)}">
-//       <img
-//         class="gallery-image"
-//         src="${link.preview.slice(1, -1)}"
-//         data-source="${link.original.slice(1, -1)}"
-//         alt="${link.description}"
-//       />
-//     </a>
-//   </li>
-//   `;
-//   gallery.insertAdjacentHTML("afterbegin", lists);
-// });
-
-// gallery.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (e.target.tagName !== "IMG") {
-//     return;
-//   }
-// });
-
-// new SimpleLightbox(".gallery a", {
-//   captionsData: "alt",
-//   captionDelay: 250,
-// });
-
-// //Style
-// gallery.style.display = "flex";
-// gallery.style.flexWrap = "wrap";
-// gallery.style.gap = "20px";
-// gallery.style.listStyle = "none";
-// gallery.style.alignItems = "center";
-// gallery.style.justifyContent = "center";
-// gallery.style.width = "1128px";
-// gallery.style.margin = "auto";
+// "id": 4611189,
+//             "pageURL": "https://pixabay.com/photos/kitten-cat-pet-feline-animal-fur-4611189/",
+//             "type": "photo",
+//             "tags": "kitten, cat, pet, feline, animal, fur, kitty, domestic, domestic cat, cat portrait, animal world, nature, cat, cat, cat, cat, cat",
+//             "previewURL": "https://cdn.pixabay.com/photo/2019/11/08/11/56/kitten-4611189_150.jpg",
+//             "previewWidth": 150,
+//             "previewHeight": 99,
+//             "webformatURL": "https://pixabay.com/get/g1ba66902ba81fdb9d7ce81ed6370a8eeeea66ec3ecd1a36aff9c68582a039fa7468c89fc8f32c0984149a8eef758b6ed960c57f15f4ad90b95deb0ae69f31cd6_640.jpg",
+//             "webformatWidth": 640,
+//             "webformatHeight": 426,
+//             "largeImageURL": "https://pixabay.com/get/g042e49262bb34d0a395ae7e4d196b83db18fd58031cd608ac7703b090a36fb6e9b64bb421af1543a8a8c1fedf79b561cbd1859690fff2cbeb882b61fa6f4f062_1280.jpg",
+//             "imageWidth": 6000,
+//             "imageHeight": 4000,
+//             "imageSize": 3885974,
+//             "views": 524471,
+//             "downloads": 408576,
+//             "collections": 14119,
+//             "likes": 1139,
+//             "comments": 188,
+//             "user_id": 9285194,
+//             "user": "guvo59",
+//             "userImageURL": ""
